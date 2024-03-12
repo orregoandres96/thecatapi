bd153a71fa510bebf90185d974e29608dff52e54
orregoandres96 <orregoandres27@gmail.com>
2024-03-12 09:43:50 -0500

    initial commit

---
+++ src/main/java/com/techtest/thecatapi/ThecatapiApplication.java
@@ -0,0 +1,13 @@
+package com.techtest.thecatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecatapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThecatapiApplication.class, args);
+	}
+
+}
